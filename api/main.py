51c5bcf89bac27dc4670c6d28e2a9bb00b478c71
BodeMurairi2 <b.murairi@alustudent.com>
2026-04-20 16:42:59 +0200

    fix CORS issues

--- api/main.py
+++ api/main.py
@@ -36,11 +36,13 @@ origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://portfolio-web-4qrj.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
